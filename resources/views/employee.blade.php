f64bb00c6b77cdcf3ccafbd48dec18e10ac9f020
lavakush7800 <lavakushgiri@gmail.com>
2021-08-27 15:59:26 +0530

    form

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -4,17 +4,29 @@
 <div class="container">
 <form method="post" action="/save">
   <div class="form-group">
-    <label for="email">Email address:</label>
-    <input type="email" class="form-control" id="email">
+    <label for="image">image:</label>
+    <input type="file" class="form-control" id="image" name="image">
   </div>
   <div class="form-group">
-    <label for="pwd">Password:</label>
-    <input type="password" class="form-control" id="pwd">
+    <label for="name">Name:</label>
+    <input type="text" class="form-control" id="name" name="name">
+  </div>
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email" name="email">
+  </div>
+  <div class="form-group">
+    <label for="mobile">Mobile:</label>
+    <input type="number" class="form-control" id="mobile" name="mobile">
+  </div>
+  <div class="form-group">
+    <label for="address">Address:</label>
+    <input type="text" class="form-control" id="address" name="address">
   </div>
   <div class="checkbox">
     <label><input type="checkbox"> Remember me</label>
   </div>
-  <button type="submit" class="btn btn-default">Submit</button>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 </div>
 @endsection
