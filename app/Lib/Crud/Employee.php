e2a41dcf1ba488956d288dccb65859b64d7fd134
lavakush7800 <lavakushgiri@gmail.com>
2021-08-29 17:30:44 +0530

    modify code

--- app/Lib/Crud/Employee.php
+++ app/Lib/Crud/Employee.php
@@ -10,7 +10,7 @@ class Employee{
             // dd($data);
             $result = Model::create($data);
             if($result){
-                return $result->toArray();
+                return $result;
             }else{
                 return '';
             }
