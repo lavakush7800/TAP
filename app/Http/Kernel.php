27daf38296642531f38b117ad08cf41e248a1854
lavakush7800 <lavakushgiri@gmail.com>
2021-09-02 16:09:39 +0530

    kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'checklogin' => \App\Http\Middleware\CheckLogin::class,
+        'admin' => \App\Http\Middleware\AdminAuthenticated::class,
+        'user'  => \App\Http\Middleware\UserAuthenticated::class,
+        'AdminAuthenticated'  => \App\Http\Middleware\AdminAuthenticated::class
     ];
 }
