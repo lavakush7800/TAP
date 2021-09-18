97f992dc475aab68511478c6700796f7d12a66d8
lavakush7800 <lavakushgiri@gmail.com>
2021-09-18 17:00:17 +0530

    modify code

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,4 +70,77 @@
         </div>
     </div>
 </div>
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header"><center><h3><b>{{ __('Register') }}</b></h3></center></div>
+                <div class="card-body bg-primary">
+                    <div class="card" style="margin-top:-20px; margin-left:-20px; margin-right:-20px;">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="form-group col">
+                                <label for="role">{{ __('Select Roles') }}</label>
+                                <select class="form-control" name="role">
+                                    <option value="">Select Role</option>
+                                    <option value="user">User</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="super_admin">Super Admin</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group col">
+                                <label for="name">{{ __('Name') }}</label>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group col">
+                                <label for="email">{{ __('Email') }}</label>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                @enderror
+                            </div>
+
+                            <div class="form-group col">
+                                <label for="password" >{{ __('Password') }}</label>
+
+                                
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                            
+                            </div>
+
+                            <div class="form-group col">
+                                <label for="password-confirm">{{ __('Confirm Password') }}</label>
+
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
+
+                            <div class="form-group col">
+                                    <button type="submit" class="btn btn-sm btn-block btn-dark">
+                                        {{ __('Register') }}
+                                    </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
