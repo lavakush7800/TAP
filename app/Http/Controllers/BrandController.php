fc25d05d5371441672c9f045f688a29116f39d9f
lacakush7800 <lavakushgiri@gmail.com>
2021-11-12 19:53:37 +0530

    make Brand and category

---
+++ app/Http/Controllers/BrandController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Brand;
+use Illuminate\Support\Carbon;
+
+class BrandController extends Controller
+{
+    public function AllBrand(){
+        $brands = Brand::latest()->paginate(5);
+        return view('admin.brand.index', compact('brands'));
+    }
+    public function StoreBrand(Request $request){
+        $validated = $request->validate([
+            'brand_name' => 'required|unique:brands|min:4',
+            'brand_image' => 'required|mimes:jpg,jpeg,png',
+        ],
+        [
+            'brand_name.required' => 'Please input Brand name',
+            'brand_name.min' => 'Brand Longer Than 4 Characters',
+        ]);
+
+        $brand_image = $request->file('brand_image');
+        $name_gen = hexdec(uniqid());
+        $img_ext = strtolower($brand_image->getClientOriginalExtension());
+        $img_name = $name_gen.'.'.$img_ext;
+        $up_location = 'image/brand/';
+        $last_img = $up_location.$img_name;
+        $brand_image->move($up_location,$img_name);
+        Brand::insert([
+            'brand_name' => $request->brand_name,
+            'brand_image' => $last_img,
+            'created_at' => Carbon::now(),
+        ]);
+
+        return Redirect()->back()->with('success','Brand Inserted Successfully');
+    }
+    public function Edit($id){
+        $brands = Brand::find($id);
+        return view('admin.brand.edit', compact('brands'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\BrandController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +21,26 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+///Category Route
+Route::get('/category/all',[CategoryController::class,'AllCat'])->name('all.category');
+Route::POST('/category/add',[CategoryController::class,'AddCat'])->name('store.category');
+Route::get('/category/edit/{id}',[CategoryController::class,'Edit']);
+Route::POST('/category/update/{id}',[CategoryController::class,'Update']);
+Route::get('/softdelete/category/{id}',[CategoryController::class,'SoftDelete']);
+Route::get('/category/restore/{id}',[CategoryController::class,'Restore']);
+Route::get('/pdelete/category/{id}',[CategoryController::class,'Pdelete']);
+
+///Brand Route
+Route::get('/brand/all',[BrandController::class,'AllBrand'])->name('all.brand');
+Route::POST('/brand/add',[BrandController::class,'StoreBrand'])->name('store.brand');
+Route::get('/brand/edit/{id}',[BrandController::class,'Edit']);
+
+
+
+
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    // $users = User::all();
+    $users = DB::table('users')->get();
+    return view('dashboard', compact('users'));
 })->name('dashboard');
